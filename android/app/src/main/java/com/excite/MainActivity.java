70d0afdca51370f0a172a7f94cae7233c1a02895
Gehan Syafesi <eugehansyafesi@yahoo.co.id>
2018-02-27 19:34:44 +0800

    add splash screen

--- android/app/src/main/java/com/excite/MainActivity.java
+++ android/app/src/main/java/com/excite/MainActivity.java
@@ -8,4 +8,15 @@ import android.widget.ImageView;
 import android.view.Gravity;
 
 public class MainActivity extends SplashActivity {
+
+    @Override
+    public LinearLayout createSplashLayout() {
+        LinearLayout view = new LinearLayout(this);
+        ImageView imageView = new ImageView(this);
+        view.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        view.setGravity(Gravity.CENTER);
+        imageView.setImageResource(R.drawable.splash_screen);
+        view.addView(imageView);
+        return view;
+    }
 }
