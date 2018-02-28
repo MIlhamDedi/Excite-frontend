d5668e4bbbb5d378464b32c81f0dd9c8c33ee9d1
Gehan Syafesi <eugehansyafesi@yahoo.co.id>
2018-02-28 16:46:27 +0800

    implement card swiping tab

--- app/screens/Swipe.js
+++ app/screens/Swipe.js
@@ -3,24 +3,85 @@ import {
   View,
   Text,
   StyleSheet,
+  ToastAndroid,
+  TouchableNativeFeedback,
 } from 'react-native';
+import Swiper from 'react-native-deck-swiper';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 class Swipe extends Component {
   render() {
+    var cardArray = ['Event A', 'Event B', 'Event C', 'Event D', 'Event E', 'Event F']
     return (
-      <View style={ styles.container } >
-        <Text>
-          Swipe
-        </Text>
-      </View>      
+      <View style={styles.container}>
+          <Swiper
+              cards={cardArray}
+              renderCard={(card) => {
+                  return (
+                      <View style={styles.card}>
+                          <Text style={styles.text}>{card}</Text>
+                      </View>
+                  )
+              }}
+              cardStyle={{
+                top: 100,
+                left: 25,
+                bottom: 125,
+                right: 25,
+                width: 'auto',
+                height: 'auto',
+              }}
+              infinite={true}
+              onSwipedLeft={(cardIndex) => {ToastAndroid.show("Swiped "+ cardArray[cardIndex] + " To the left" , ToastAndroid.SHORT)}}
+              onSwipedRight={(cardIndex) => {ToastAndroid.show("Swiped "+ cardArray[cardIndex] + " To the Right", ToastAndroid.SHORT)}}
+              onSwipedTop={(cardIndex) => {ToastAndroid.show("Swiped "+ cardArray[cardIndex] + " To the Top", ToastAndroid.SHORT)}}
+              onSwipedBottom={(cardIndex) => {ToastAndroid.show("Swiped "+ cardArray[cardIndex] + " To the Bottom", ToastAndroid.SHORT)}}
+              onSwipedAll={() => {ToastAndroid.show("That's all", ToastAndroid.SHORT)}}
+              onTapCard={(cardIndex) => {ToastAndroid.show("Not for tapping (˵>﹏<˵) ", ToastAndroid.SHORT)}}
+              cardIndex={0}
+              backgroundColor={'#f44336'}>
+              <View style={{paddingTop: 30, alignItems: 'flex-end', right: 25}} >
+                <View style={{width: 24, height:24, borderRadius: 12, overflow: "hidden"}}>
+                  <TouchableNativeFeedback
+                    useForeground={true}
+                    onPress={() => {ToastAndroid.show("Info will be shown with this button", ToastAndroid.SHORT)}}>
+                    <View style={{backgroundColor: "#FAFAFA", borderRadius: 12}}>
+                      <Icon name="info" size={24} color="#f44336" />
+                    </View>
+                  </TouchableNativeFeedback>
+                </View>
+              </View>
+          </Swiper>
+      </View>
     )
   }
 }
+export default Swipe;
 
 const styles = StyleSheet.create({
-  container:{
-    paddingTop: 30,
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
   },
-});
-
-export default Swipe;
+  card: {
+    flex: 1,
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: '#E8E8E8',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 50,
+    backgroundColor: 'transparent'
+  },
+  button:{
+    width: 50,
+    height: 30,
+    alignItems: 'center', 
+    justifyContent: 'center', 
+    backgroundColor: '#2196F3',
+    borderRadius: 10,
+  }
+})
